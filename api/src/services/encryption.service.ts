dbdf38f53d34c8021e86fcf1651dafcf9b440c6b
Ben Matela <matelaben2@gmail.com>
2024-11-04 13:39:14 +0200

    doc update

--- api/src/services/encryption.service.ts
+++ api/src/services/encryption.service.ts
@@ -28,9 +28,7 @@ export const encrypt = async (fileToEncryptPath: string, aesBlockSize: AESBlockS
         /**
          * Create a hash of the password we'll be using to encrypt files.
          * 
-         * It is one-way, meaning it’s very difficult, given a hash, to reverse it and figure out what went in.
-         * It produces a fixed output length. For sha256, it will always produce a 32 byte buffer, which just happens to be the size we needed for our AES-256 cipher.
-         * It’s deterministic. That is, the hash function will always produce the same hash for the same plaintext.
+        
          */
         const hash = crypto.createHash(EncryptionAlgorithm.SHA_256);
         hash.update("mySuperSecretEncryptionPassword");
@@ -63,6 +61,15 @@ export const encrypt = async (fileToEncryptPath: string, aesBlockSize: AESBlockS
 
 /**
  * Easily get a cipher key for any password.
+ * 
+ * It is `one-way`, meaning it’s very difficult, given a hash, to reverse it and 
+ * figure out what went in.
+ * 
+ * It produces a fixed output length. For `sha256`, it will always produce a `32` byte 
+ * buffer, which just happens to be the size we needed for our `AES-256` cipher.
+ * 
+ * It’s deterministic. That is, the hash function will `always` produce the same hash for 
+ * the same plaintext.
  * 
  * @param {string} password
  *  
