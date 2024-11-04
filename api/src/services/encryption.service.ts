dacdb9d56006bc9b8f3f8066c4a42bf569605236
Ben Matela <matelaben2@gmail.com>
2024-11-04 11:53:33 +0200

    doc encrypt function

--- api/src/services/encryption.service.ts
+++ api/src/services/encryption.service.ts
@@ -7,17 +7,18 @@ const NAMESPACE = 'ENCRYPTION SERVICE';
 /**
  * Encrypts a file using a chosen AES block size
  * 
+ * More info: https://www.moserware.com/2009/09/stick-figure-guide-to-advanced.html
+ * 
  * @param {string} fileToEncryptPath 
  * @param {AESBlockSize} aesBlockSize 
  */
 export const encrypt = (fileToEncryptPath: string, aesBlockSize: AESBlockSize) => {
     try {
-        /**
-         * Streams are a powerful tool that allows us to write programs which deal with 
-         * small amounts of data in an asynchronous manner.
-         */
+        // Streams are a powerful tool that allows us to write programs which deal with 
+        // small amounts of data in an asynchronous manner.
         const readStream = fs.createReadStream('./file.txt');
 
+        // When we receive data chunk from the stream, process them
         readStream.on('data', (chunk) => {
             console.log(chunk.toString('utf8'));
         });
