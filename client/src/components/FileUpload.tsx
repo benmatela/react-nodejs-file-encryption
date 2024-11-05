7c1f6d3341babc4182c48d470053d272b4ed7c27
Ben Matela <matelaben2@gmail.com>
2024-11-05 17:55:38 +0200

    init upload component

---
+++ client/src/components/FileUpload.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+
+export const FileUpload = () => {
+    const [selectedFile, setSelectedFile] = useState<File>();
+    const [uploadStatus, setUploadStatus] = useState<string>("");
+    const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+    /**
+     * Handles file select change
+     * 
+     * @param {React.ChangeEvent<HTMLInputElement>} event 
+     */
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event && event.target && event.target.files) {
+            const file = event.target.files[0];
+            setSelectedFile(file);
+        }
+    };
+
+    const handleFileUpload = () => {
+        try {
+            if (!selectedFile) {
+                alert("Please select a file to upload.");
+                return;
+            }
+
+            const chunkSize = 5 * 1024 * 1024; // 5MB (adjust based on your requirements)
+            const totalChunks = Math.ceil(selectedFile.size / chunkSize);
+            const chunkProgress = 100 / totalChunks;
+            let chunkNumber = 0;
+            let start = 0;
+            let end = 0;
+
+            const uploadNextChunk = async () => {
+                if (end <= selectedFile.size) {
+                    const chunk = selectedFile.slice(start, end);
+                    const formData = new FormData();
+                    formData.append("file", chunk);
+                    formData.append("chunkNumber", String(chunkNumber));
+                    formData.append("totalChunks", String(totalChunks));
+                    formData.append("originalname", selectedFile.name);
+
+                    fetch("http://localhost:8000/upload", {
+                        method: "POST",
+                        body: formData,
+                    })
+                        .then((response) => response.json())
+                        .then((data) => {
+                            console.log({ data });
+                            const temp = `Chunk ${chunkNumber + 1
+                                }/${totalChunks} uploaded successfully`;
+                            setUploadStatus(temp);
+                            setUploadProgress(Number((chunkNumber + 1) * chunkProgress));
+                            console.log(temp);
+                            chunkNumber++;
+                            start = end;
+                            end = start + chunkSize;
+                            uploadNextChunk();
+                        })
+                        .catch((error: Error) => {
+                            console.error("Error uploading chunk:", error.message);
+                        });
+                } else {
+                    setUploadProgress(100);
+                    setSelectedFile(undefined);
+                    setUploadStatus("File upload completed");
+                }
+            };
+
+            uploadNextChunk();
+        } catch (error: any) {
+            console.error(error.message);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Resumable File Upload</h2>
+            <h3>{uploadStatus}</h3>
+            {uploadProgress > 0 && <progress value={uploadProgress} />}
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleFileUpload}>Upload File</button>
+        </div>
+    );
+}
