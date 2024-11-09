0ee3ce3e8e2b31942fc1c835ad46f47177a85fb3
Ben Matela <matelaben2@gmail.com>
2024-11-09 09:42:55 +0200

    init encryption

--- client/src/pages/encryption/Encryption.tsx
+++ client/src/pages/encryption/Encryption.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FileUpload } from '../../components/FileUpload'
+import { AESBlockSize } from '../../models/enums/encryption.enum';
 
 export const Encryption = () => {
-  return (
-    <div>Encryption</div>
-  )
+    const [shouldEncrypt, setShouldEncrypt] = useState<boolean>(true);
+    const [aesBlockSize, setAesBlockSize] = useState<AESBlockSize>(AESBlockSize.AES_256);
+    const [encryptionPassword, setEncryptionPassword] = useState<string>("");
+    return (
+        <div>
+            <h3>Encryption</h3>
+            <FileUpload
+                shouldEncrypt={shouldEncrypt}
+                aesBlockSize={aesBlockSize}
+                encryptionPassword={encryptionPassword}
+            />
+        </div>
+    )
 }
