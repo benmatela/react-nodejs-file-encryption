2608d7a737697b2f6a8253f6c2748aca86682c98
Ben Matela <matelaben2@gmail.com>
2024-11-09 07:49:13 +0200

    update

--- client/src/models/enums/http-request-method.enum.ts
+++ client/src/models/enums/http-request-method.enum.ts
@@ -6,26 +6,26 @@ export enum HTTP_REQUEST_METHOD {
      * The POST method submits an entity to the specified resource, 
      * often causing a change in state or side effects on the server.
      */
-    POST = "",
+    POST = "POST",
     /**
      * The GET method requests a representation of the specified resource.
      */
-    GET = "",
+    GET = "GET",
     /**
      * The DELETE method deletes the specified resource.
      */
-    DELETE = "",
+    DELETE = "DELETE",
     /**
      * The PUT method replaces all current representations of the target 
      * resource with the request content.
      */
-    PUT = "",
+    PUT = "PUT",
     /**
      * The PATCH method applies partial modifications to a resource.
      */
-    PATCH = "",
+    PATCH = "PATCH",
     /**
      * The OPTIONS method describes the communication options for the target resource.
      */
-    OPTIONS = ""
+    OPTIONS = "OPTIONS"
 }
